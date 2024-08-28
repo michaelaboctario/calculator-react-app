b0997c0697e961d052b8b1501a28c5fcaac37758
Michaela Bucatariu <mbucatariu24@gmail.com>
2024-08-28 17:37:02 +0200

    Add buttons for keys

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,15 @@ import styles from "./page.module.css";
 import Container from "@/_components/container"
 import ButtonCard from "@/_components/buttoncard";
 import Display from "@/_components/display";
+import Button from "@/_components/button";
+
+const btnSymbols = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "=", ""],
+];
 
 export default function Home() {
   return (
@@ -15,7 +24,22 @@ export default function Home() {
         </div>
         <Container>
           <Display value="0"></Display>
-          <ButtonCard></ButtonCard>
+          <ButtonCard>
+          {
+            btnSymbols.flat().map((btn, i) => {
+              return (
+                <Button
+                  key={i}
+                  className={btn === "=" ? "button-two" : ""}
+                  value={btn}
+                  /* onClick={() => {
+                    console.log(`${btn} clicked!`);
+                  }} */
+                />
+              );
+            })
+          }
+          </ButtonCard>
         </Container>
         
     </main>
